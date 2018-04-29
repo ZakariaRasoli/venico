5d042cd2a2107e5e2693c71b7bae3d96365229eb
ZakariaRasoli <zakaria.rasoli@Gmail.com>
2018-04-29 13:14:31 +0430

    fixed v13

--- lockplus.php
+++ lockplus.php
@@ -1,11 +1,5 @@
 <?php
-/*
-in the name of God 
-source of anti spam bot
-by php 
-v.13
-dev : mohammadrezajafari [@mohammadrezajiji]
-*/
+
 // voice
 if($textmassage=="/lock voice" or $textmassage=="قفل ویس"){
 if ( $status == 'creator' or $status == 'administrator' or in_array($from_id,$Dev) ){
