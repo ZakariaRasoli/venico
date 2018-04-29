22c069f6ce51df1ac8f652e75c486b68e51c3224
ZakariaRasoli <zakaria.rasoli@Gmail.com>
2018-04-29 13:19:31 +0430

    fixed v13

--- plusupmsgcheck.php
+++ plusupmsgcheck.php
@@ -1,11 +1,5 @@
 <?php
-/*
-in the name of God 
-source of anti spam bot
-by php 
-v.13
-dev : mohammadrezajafari [@mohammadrezajiji]
-*/
+
 // restart settings 
 if($settings["information"]["step"] == "reset"){
 if($textmassage == "بله"){
